57357e138cfc5bb5e6b7e813ee10d39714f20c3d
David Siegel <djsiegel@gmail.com>
2016-12-14 10:11:12 -0800

    Tidy for public consumption

--- fsanimals/Program.fs
+++ fsanimals/Program.fs
@@ -1,9 +1,7 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
-
-open System.IO
+﻿open System.IO
 
 let rand = System.Random()
+
 let animals =
     File.ReadAllLines("../../animals.txt")
     |> Array.map (fun s -> s.ToLower())
@@ -12,23 +10,13 @@ let adjectives =
     File.ReadAllLines("../../adjectives.txt")
     |> Array.map (fun s -> s.ToLower())
 
-let adjectivesByLetter =
-    adjectives
-    |> Array.groupBy (fun s -> s.[0])
-    |> dict
-
 let getAdjectiveAnimal() =
-    let animal = animals.[rand.Next(animals.Length)]
-    let adjective = adjectives.[rand.Next(adjectives.Length)]
-    (adjective, animal)
+    let random (array : 'a[]) = array.[rand.Next(array.Length)]
+    (random adjectives, random animals)
 
-let pairs = seq {
-    while true do
-        yield getAdjectiveAnimal()
-}
+let pairs = Seq.initInfinite (fun _ -> getAdjectiveAnimal())
 
 [<EntryPoint>]
 let main argv =
     printfn "%A" (pairs |> Seq.take 100 |> List.ofSeq)
-    0 // return an integer exit code
-
+    0
